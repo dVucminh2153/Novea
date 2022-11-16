51ca0936a40cc72f137ade73d56be390d6bd9697
Admin <Admin@SHMEBULOCK>
2022-11-16 23:33:48 +0700

    Add project files.

---
+++ Đồ Án Lập Trình Trực Quan/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Đồ_Án_Lập_Trình_Trực_Quan
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
